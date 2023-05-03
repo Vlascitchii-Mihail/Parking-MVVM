51f70858c40de2db09c692ba07294ccd7c080fea
AnatolieStambulo <stambulo.anatolii@gmail.com>
2023-05-03 18:44:23 +0300

    Create Splash Screen

--- app/src/main/java/com/endava/parking/MainActivity.kt
+++ app/src/main/java/com/endava/parking/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.endava.parking
 
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
