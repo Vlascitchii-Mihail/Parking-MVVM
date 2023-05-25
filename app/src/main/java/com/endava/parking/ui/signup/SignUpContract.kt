5959678bc5e463a4a07820d467c89b39acbd725d
AnatolieStambulo <stambulo.anatolii@gmail.com>
2023-05-25 17:48:36 +0300

    Correct after code review

--- app/src/main/java/com/endava/parking/ui/signup/SignUpContract.kt
+++ app/src/main/java/com/endava/parking/ui/signup/SignUpContract.kt
@@ -7,11 +7,19 @@ interface SignUpContract {
     interface View {
 
         fun setButtonAvailability(isEnable: Boolean)
+
+        fun showErrorMessage(fieldType: InputTextType, errorMessageId: Int)
+
+        fun clearErrorMessage(fieldType: InputTextType)
+
+        fun moveFocusTo(nextFocusedField: InputTextType?)
     }
 
     interface Presenter {
 
         fun checkUserValidation(user: User)
+
+        fun validateField(fieldType: InputTextType, text: CharSequence)
 
         fun submitData(user: User)
     }
