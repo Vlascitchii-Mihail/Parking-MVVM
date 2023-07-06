ef5aace4f5e6a9268dc8ce4cee1201debb899b1a
AnatolieStambulo <stambulo.anatolii@gmail.com>
2023-07-06 09:04:20 +0300

    ParkingLots, Retrofit (with third party backend)

---
+++ app/src/main/java/com/endava/parking/data/model/GithubUser.kt
@@ -0,0 +1,9 @@
+package com.endava.parking.data.model
+
+/**  Only for test  */
+data class GithubUser(
+    val id: String,
+    val login: String,
+    val avatarUrl: String,
+    val reposUrl: String
+)
