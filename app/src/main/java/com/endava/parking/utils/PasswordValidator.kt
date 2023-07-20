a4777dd958afea42afce79819815abb7e4c05b49
Anatolie Stambulo <anatolie.stambulo@endava.com>
2023-07-20 08:10:03 +0000

    astambulo-password-validation-bug-fix

--- app/src/main/java/com/endava/parking/utils/PasswordValidator.kt
+++ app/src/main/java/com/endava/parking/utils/PasswordValidator.kt
@@ -3,14 +3,11 @@ package com.endava.parking.utils
 import java.util.regex.Pattern
 import javax.inject.Inject
 
-private const val PASSWORD_REGEX = "^(?=.*[A-Z])(?=.*?[0-9])(?=.*?[!@#$%^&*()_+*\"':;?])[A-Za-z0-9!@#\$%^&*()_+*\"':;?]{5,10}$"
+private const val PASSWORD_REGEX =
+    "^(?=.*[A-Z])(?=.*?[0-9])(?=.*?[`~!@#$%^&*()_+=\"':;?\\\\/])[A-Za-z0-9`~!@#\$%^&*()_+=\"':;?\\\\/]{5,10}$"
 
 class PasswordValidator @Inject constructor() : Validator {
 
-    override fun validate(password: CharSequence): Boolean {
-        val regex = PASSWORD_REGEX
-        val pattern = Pattern.compile(regex)
-        val matcher = pattern.matcher(password)
-        return matcher.matches()
-    }
+    override fun validate(password: CharSequence): Boolean =
+        Pattern.compile(PASSWORD_REGEX).matcher(password).matches()
 }
